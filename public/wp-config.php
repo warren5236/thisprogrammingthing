c11342ddd2d2fbf1194451892da22d8a71280a22
Scott Warren <warren5236@gmail.com>
2013-10-06 17:04:32 -0400

    Hopefully the last update for wp-config.

--- public/wp-config.php
+++ public/wp-config.php
@@ -29,6 +29,8 @@ if(is_file('../../wp-config-local.php')){
     require_once('../../wp-config-local.php');
 } else if(is_file('../wp-config-local.php')){
     require_once('../wp-config-local.php');
+} else if(is_file('../../../wp-config-local.php')){
+    require_once('../../../wp-config-local.php');
 }
 
 /** MySQL hostname */
